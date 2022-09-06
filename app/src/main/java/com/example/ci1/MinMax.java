5e8a0571abb13a13e78d5bb39b9aa96dc661498e
ktblsva <grantired.jpg@gmail.com>
2022-09-06 22:56:26 +0700

    add MinMax class

--- app/src/main/java/com/example/ci1/MinMax.java
+++ app/src/main/java/com/example/ci1/MinMax.java
@@ -1,18 +1,6 @@
 package com.example.ci1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
+public class MinMax {
     /**
      * min function
      * @param a first param
